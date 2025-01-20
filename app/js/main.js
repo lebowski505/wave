9ff0147ad0936c4e749f8fc8f8cb200da6b3686e
lebowski505 <dmitriysalienko@gmail.com>
2025-01-20 15:43:58 +0200

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
-$ (function(){
+$(function () {
+  
+    $('.blog-slider').slick({
+      dots: true,
+      arrows: false,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  
+  
+
+  const mixer = mixitup('.gallery__content');
+  
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, watch, parallel, series} = require('gulp');
+const { src, dest, watch, parallel, series } = require('gulp');
 const scss         = require('gulp-sass')(require('sass'));
 const concat       = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer'); 
@@ -7,82 +7,86 @@ const del          = require('del');
 const imagemin     = require('gulp-imagemin'); 
 const browserSync  = require('browser-sync').create(); 
 
+
 function browsersync() {
   browserSync.init({
     server: {
-      baseDir: 'app/'
+      baseDir: 'app/' 
     },
-    notify: false
+    notify: false,
+    reloadDebounce: 500 
   })
 }
 
 
 function styles() {
-  return src('app/scss/style.scss')
-    .pipe(scss({ outputStyle: 'compressed' }).on('error', function(err) {
-      console.error('SCSS Error:', err.message); 
-      this.emit('end'); 
-    }))
+  return src([
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css', 
+    'app/scss//*.scss'
+  ]) 
+    .pipe(scss({ outputStyle: 'compressed' }).on('error', scss.logError))
     .pipe(concat('style.min.css'))
+    .pipe(imagemin([ 
+      imagemin.mozjpeg({ quality: 75, progressive: true, fast: true })
+    ]))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions'],
       grid: true
     }))
-    .pipe(dest('app/css'))
-    .pipe(browserSync.stream());
+    .pipe(dest('app/css')) 
+    .pipe(browserSync.stream()); 
 }
+
 
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/mixitup/dist/mixitup.min.js',
     'app/js/main.js'
   ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
-  .pipe(browserSync.stream())
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream());
 }
 
-function images(){
-  return src('app/images/**/*.*')
-  .pipe(imagemin([
-    imagemin.gifsicle({interlaced: true}),
-    imagemin.mozjpeg({quality: 75, progressive: true}),
-    imagemin.optipng({optimizationLevel: 5}),
-    imagemin.svgo({
-      plugins: [
-        {
-          name: 'removeViewBox',
-          active: true
-        },
-        {
-          name: 'cleanupIDs',
-          active: false
-        }
-      ]
-    })
-  ]))
-  .pipe(dest('dist/images'))
+function images() {
+  return src('app/images//*.*')
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ quality: 75, progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
+        plugins: [
+          { name: 'removeViewBox', active: true },
+          { name: 'cleanupIDs', active: false }
+        ]
+      })
+    ]))
+    .pipe(dest('dist/images'));
 }
 
 function build() {
   return src([
-    'app/**/*.html',
+    'app//*.html',
     'app/css/style.min.css',
     'app/js/main.min.js'
-  ], {base: 'app'})
-  .pipe(dest('dist'))
+  ], { base: 'app' })
+    .pipe(dest('dist'));
 }
 
 function cleanDist() {
-  return del('dist')
+  return del('dist');
 }
 
 function watching() {
-  watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-  watch(['app/**/*.html']).on('change', browserSync.reload);
+  watch(['app/scss//*.scss'], styles);
+  watch(['app/js//*.js', '!app/js/main.min.js'], scripts); 
+  watch(['app//*.html']).on('change', browserSync.reload);
 }
+
 
 
 exports.styles      = styles;
@@ -94,4 +98,3 @@ exports.cleanDist   = cleanDist;
 exports.build       = series(cleanDist, images, build);
 
 exports.default = parallel(styles, scripts, browsersync, watching);
-
